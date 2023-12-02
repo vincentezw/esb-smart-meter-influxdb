2b709d401c8e269856e81682f1ba3839c6d4eb81
Vincent Elger Zwanenburg <vincent@zwanenburg.ie>
2023-12-02 02:10:25 +0000

    use class, deduplicate on date

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # esb-smart-meter-reading-automation
 
 ![](https://github.com/badger707/esb-smart-meter-reading-automation/blob/main/esb-smart-meter.png)
-<br><br>
 
 # How to read your Smart Meter data automatically?
 
-<br>
 Since I've got smart meter installed, I was looking for a way to automatically collect my meter data to track electricity usage (and solar export) with corresponding pricing as per current supplier rates.<br><br>
 While searching on internet I found this post https://www.boards.ie/discussion/2058292506/esb-smart-meter-data-script as potential candidate to start with.
-<br>
+
 Unfortunatelly linked script is broken - ESB have chnaged some URL's and file structure since then and I had to spend some time and tinker with code to make it working with new (as of writing it is 21-JUL-2023) URL structure.<br><br>
 End result - code is fixed and runs just fine now, I am able to read all smart meter readings from my account in JSON format and push it further to my InfluxDB and Home Assistant for analysis/reporting.
-<br><br>
-# Requirements<br>
-* You need to create account with ESB here https://myaccount.esbnetworks.ie <br>
-* In your account, link your electricity meter MPRN
-<br><br>
-# Script setup<br>
-* Create a file `.secrets` in this folder with your login details for ESB networks and InfluxDB. This needs to be in the following format:
+
+# Requirements
+
+- You need to create account with ESB here https://myaccount.esbnetworks.ie
+- In your account, link your electricity meter MPRN
+
+# Script setup
+
+Create a file `.secrets` in this folder with your login details for ESB networks and InfluxDB. This needs to be in the following format:
 
 ```
 [ESB]
@@ -38,4 +38,4 @@ I hope this will be usefull, cheers!
 
 # Usage
 
-Run the script :-), You can use the `--debug`` flag to show a more verbose output. You may need to install some stuff like `influxdb3-python`- I use a virtual environment and`pip` to install.
+Run the script :-), You can use the `--debug` flag to show a more verbose output. You may need to install some stuff like `influxdb3-python`- I use a virtual environment and`pip` to install.
--- esb-smart-meter-reader.py
+++ esb-smart-meter-reader.py
@@ -14,9 +14,10 @@ import configparser
 import csv
 import json
 import logging
+import os
 import re
 import sys
-from datetime import datetime, timedelta, timezone
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
@@ -31,138 +32,170 @@ if args.debug:
     logging.getLogger().setLevel(logging.DEBUG)
     logging.info("Debug mode enabled.")
 
-logging.debug("reading config")
-config = configparser.ConfigParser()
-config.read(".secrets")
 
-esb_meter_mprn = config.get("ESB", "meter_mprn")
-esb_username = config.get("ESB", "username")
-esb_password = config.get("ESB", "password")
-influx_host = config.get("InfluxDB", "host")
-influx_bucket = config.get("InfluxDB", "bucket")
-influx_organisation = config.get("InfluxDB", "organisation")
-influx_token = config.get("InfluxDB", "token")
+class ESBSmartMeterReader:
+    json_file_path = "esb_readings.json"
+
+    def __init__(
+        self,
+        config_file_path,
+    ):
+        logging.debug("reading config")
+        config = configparser.ConfigParser()
+        config.read(config_file_path)
+        self.esb_meter_mprn = config.get("ESB", "meter_mprn")
+        self.esb_username = config.get("ESB", "username")
+        self.esb_password = config.get("ESB", "password")
+        self.influx_host = config.get("InfluxDB", "host")
+        self.influx_bucket = config.get("InfluxDB", "bucket")
+        self.influx_organisation = config.get("InfluxDB", "organisation")
+        self.influx_token = config.get("InfluxDB", "token")
+
+    def load_smart_meter_stats_v2(self):
+        # this parameter looks to be ignored by the ESB endpoint, which always returns *all* the data
+        today_ = datetime.today()
+        smart_meter_data = self.__load_esb_data(today_)
+        if len(smart_meter_data) == 0:
+            logging.info("No new data found")
+            return
+
+        self.__export_to_influx(smart_meter_data)
+        logging.info("Import completed and JSON file generated")
+
+    def __load_esb_data(self, start_date):
+        logging.info("Loading ESB data for MPRN %s", self.esb_meter_mprn)
+        s = requests.Session()
+        s.headers.update(
+            {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+            }
+        )
+        logging.debug("[+] calling login page. ..")
+        login_page = s.get("https://myaccount.esbnetworks.ie/", allow_redirects=True)
+        result = re.findall(r"(?<=var SETTINGS = )\S*;", str(login_page.content))
+        settings = json.loads(result[0][:-1])
+        logging.debug("[+] sending credentials ...")
+        s.post(
+            "https://login.esbnetworks.ie/esbntwkscustportalprdb2c01.onmicrosoft.com/B2C_1A_signup_signin/SelfAsserted?tx="
+            + settings["transId"]
+            + "&p=B2C_1A_signup_signin",
+            data={
+                "signInName": self.esb_username,
+                "password": self.esb_password,
+                "request_type": "RESPONSE",
+            },
+            headers={
+                "x-csrf-token": settings["csrf"],
+            },
+            allow_redirects=False,
+        )
+        logging.debug("[+] passing AUTH ...")
+        confirm_login = s.get(
+            "https://login.esbnetworks.ie/esbntwkscustportalprdb2c01.onmicrosoft.com/B2C_1A_signup_signin/api/CombinedSigninAndSignup/confirmed",
+            params={
+                "rememberMe": False,
+                "csrf_token": settings["csrf"],
+                "tx": settings["transId"],
+                "p": "B2C_1A_signup_signin",
+            },
+        )
+        logging.debug("[+] confirm_login: ", confirm_login)
+        logging.debug("[+] doing some BeautifulSoup ...")
+        soup = BeautifulSoup(confirm_login.content, "html.parser")
+        form = soup.find("form", {"id": "auto"})
+        s.post(
+            form["action"],
+            allow_redirects=False,
+            data={
+                "state": form.find("input", {"name": "state"})["value"],
+                "client_info": form.find("input", {"name": "client_info"})["value"],
+                "code": form.find("input", {"name": "code"})["value"],
+            },
+        )
+
+        logging.debug("[+] getting CSV file for MPRN ...")
+        data = s.get(
+            "https://myaccount.esbnetworks.ie/DataHub/DownloadHdf?mprn="
+            + self.esb_meter_mprn
+            + "&startDate="
+            + start_date.strftime("%Y-%m-%d")
+        )
+
+        logging.debug("[+] CSV file received !!!")
+        data_decoded = data.content.decode("utf-8").splitlines()
+        logging.debug("[+] data decoded from Binary format")
+        json_data = self.__csv_response_to_json(data_decoded)
+        return json_data
+
+    def __csv_response_to_json(self, csv_file):
+        logging.debug("[+] creating JSON file from CSV ...")
+        output_json = []
+        existing_data = self.__get_previous_data()
+        new_entries = []
+        existing_entries = set(
+            (entry["Read Date and End Time"] for entry in existing_data)
+        )
+        logging.debug("Found %s existing records in JSON file", len(existing_entries))
+
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            output_json.append(row)
+            unique_identifier = row["Read Date and End Time"]
+            if unique_identifier not in existing_entries:
+                new_entries.append(row)
+                existing_entries.add(unique_identifier)
+            else:
+                print(row)
+
+        logging.info(
+            "Found %s new entries (%s entries downloaded)",
+            len(new_entries),
+            len(output_json),
+        )
+
+        with open(self.json_file_path, "w", encoding="utf-8") as jsonf:
+            json_out = json.dumps(output_json, indent=2)
+            jsonf.write(json_out)
+            logging.debug("[+] JSON file created")
+        return new_entries
+
+    def __get_previous_data(self):
+        existing_json_data = []
+
+        if os.path.exists(self.json_file_path):
+            with open(self.json_file_path, "r", encoding="utf-8") as existing_json_file:
+                existing_json_data = json.load(existing_json_file)
+
+        return existing_json_data
+
+    def __export_to_influx(self, data):
+        client = InfluxDBClient3(
+            token=self.influx_token,
+            host=self.influx_host,
+            org=self.influx_organisation,
+            database=self.influx_bucket,
+        )
+
+        total_entries = len(data)
+
+        for i, entry in enumerate(data, 1):
+            timestamp = datetime.strptime(
+                entry["Read Date and End Time"], "%d-%m-%Y %H:%M"
+            )
+            point = (
+                Point("meter_reading")
+                .tag("MPRN", entry["MPRN"])
+                .tag("MeterSerialNumber", entry["Meter Serial Number"])
+                .tag("ReadType", entry["Read Type"])
+                .field("reading", float(entry["Read Value"]))
+                .time(timestamp)
+            )
+            client.write(point)
+            sys.stdout.write(f"\rProcessing entry {i}/{total_entries}")
+            sys.stdout.flush()
+
+        client.close()
+        print()
 
 
-def load_esb_data(user, password, mpnr, start_date):
-    logging.info("Loading ESB data for MPRN %s", mpnr)
-    s = requests.Session()
-    s.headers.update(
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-        }
-    )
-    logging.debug("[+] calling login page. ..")
-    login_page = s.get("https://myaccount.esbnetworks.ie/", allow_redirects=True)
-    result = re.findall(r"(?<=var SETTINGS = )\S*;", str(login_page.content))
-    settings = json.loads(result[0][:-1])
-    logging.debug("[+] sending credentials ...")
-    s.post(
-        "https://login.esbnetworks.ie/esbntwkscustportalprdb2c01.onmicrosoft.com/B2C_1A_signup_signin/SelfAsserted?tx="
-        + settings["transId"]
-        + "&p=B2C_1A_signup_signin",
-        data={"signInName": user, "password": password, "request_type": "RESPONSE"},
-        headers={
-            "x-csrf-token": settings["csrf"],
-        },
-        allow_redirects=False,
-    )
-    logging.debug("[+] passing AUTH ...")
-    confirm_login = s.get(
-        "https://login.esbnetworks.ie/esbntwkscustportalprdb2c01.onmicrosoft.com/B2C_1A_signup_signin/api/CombinedSigninAndSignup/confirmed",
-        params={
-            "rememberMe": False,
-            "csrf_token": settings["csrf"],
-            "tx": settings["transId"],
-            "p": "B2C_1A_signup_signin",
-        },
-    )
-    logging.debug("[+] confirm_login: ", confirm_login)
-    logging.debug("[+] doing some BeautifulSoup ...")
-    soup = BeautifulSoup(confirm_login.content, "html.parser")
-    form = soup.find("form", {"id": "auto"})
-    s.post(
-        form["action"],
-        allow_redirects=False,
-        data={
-            "state": form.find("input", {"name": "state"})["value"],
-            "client_info": form.find("input", {"name": "client_info"})["value"],
-            "code": form.find("input", {"name": "code"})["value"],
-        },
-    )
-
-    # data = s.get('https://myaccount.esbnetworks.ie/datadub/GetHdfContent?mprn=' + mpnr + '&startDate=' + start_date.strftime('%Y-%m-%d'))
-    logging.debug("[+] getting CSV file for MPRN ...")
-    data = s.get(
-        "https://myaccount.esbnetworks.ie/DataHub/DownloadHdf?mprn="
-        + mpnr
-        + "&startDate="
-        + start_date.strftime("%Y-%m-%d")
-    )
-
-    logging.debug("[+] CSV file received !!!")
-    data_decoded = data.content.decode("utf-8").splitlines()
-    logging.debug("[+] data decoded from Binary format")
-    json_data = csv_response_to_json(data_decoded)
-    return json_data
-
-
-def csv_response_to_json(csv_file):
-    logging.debug("[+] creating JSON file from CSV ...")
-    my_json = []
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        my_json.append(row)
-    with open("json_data.json", "w", encoding="utf-8") as jsonf:
-        json_out = json.dumps(my_json, indent=2)
-        jsonf.write(json_out)
-    logging.debug("[+] end of JSON OUT, returning value ...")
-    return my_json
-
-
-def parse_date(date_str):
-    logging.debug("[+] parsing some data fields ...")
-    if len(date_str) == 19:
-        return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")
-    else:
-        dt = datetime.strptime(date_str[:19], "%Y-%m-%dT%H:%M:%S")
-        tz_offset = int(date_str[-6:-3])
-        tz = timezone(timedelta(hours=tz_offset))
-        return dt.replace(tzinfo=tz)
-
-
-def load_smart_meter_stats_v2(user, password, mpnr):
-    # last_month = datetime.today() - timedelta(days=30)
-    today_ = datetime.today()
-    # smart_meter_data = load_esb_data(user, password, mpnr, last_month)
-    smart_meter_data = load_esb_data(user, password, mpnr, today_)
-    return smart_meter_data
-
-
-client = InfluxDBClient3(
-    token=influx_token,
-    host=influx_host,
-    org=influx_organisation,
-    database=influx_bucket,
-)
-
-data = load_smart_meter_stats_v2(esb_username, esb_password, esb_meter_mprn)
-total_entries = len(data)
-
-for i, entry in enumerate(data, 1):
-    timestamp = datetime.strptime(entry["Read Date and End Time"], "%d-%m-%Y %H:%M")
-    point = (
-        Point("meter_reading")
-        .tag("MPRN", entry["MPRN"])
-        .tag("MeterSerialNumber", entry["Meter Serial Number"])
-        .tag("ReadType", entry["Read Type"])
-        .field("reading", float(entry["Read Value"]))
-        .time(timestamp)
-    )
-    client.write(point)
-    sys.stdout.write(f"\rProcessing entry {i}/{total_entries}")
-    sys.stdout.flush()
-
-client.close()
-print()
-logging.info("Import completed and JSON file generated")
+ESBSmartMeterReader(".secrets").load_smart_meter_stats_v2()
